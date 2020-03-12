b8c613c9ab929226b366a178a687e2b19472814a
lxnlxn <645321024@qq.com>
2020-03-12 16:25:30 +0800

    Initial commit

---
+++ src/main/java/com/com/gmalltest/GmallTestApplication.java
@@ -0,0 +1,13 @@
+package com.com.gmalltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallTestApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/com/gmalltest/test/hello.java
@@ -0,0 +1,18 @@
+package com.com.gmalltest.test;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author lxn
+ * @create 2020-03-12 16:10
+ */
+public class hello {
+    public static void main(String[] args) {
+
+        String a = "1";
+        System.out.println(a);
+
+    }
+
+
+}
